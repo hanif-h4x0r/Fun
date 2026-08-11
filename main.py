9d31da7be3f81830dffa869454283c6f415339ca
Hanif <hanifabdurrahmanarifai@gmail.com>
2026-08-11 19:55:55 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import os
-x = os.system('clear')
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI is running!"}
+   
+@app.get("/users/{user_id}")
+def read_user(user_id: int, q: str | None = None):
+    return {"user_id": user_id, "q": q}
+    
